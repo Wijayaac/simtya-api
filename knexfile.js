53f3aaaf4689b42531633ebb281dc37b0df40be4
Wijayaac <wijayaac25@gmail.com>
2021-06-08 10:39:34 +0800

    Xconfiguring knexfile.js for configuring

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: process.env.POSTGRES_URL,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations",
     },
