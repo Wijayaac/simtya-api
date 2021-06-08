002e369f7c7ff8f0f94644129d26966410cdae9c
Wijayaac <wijayaac25@gmail.com>
2021-06-08 10:27:22 +0800

    change listen port for deploy

--- app.js
+++ app.js
@@ -32,4 +32,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes"));
 
 // Server listens on http://localhost:3000
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
